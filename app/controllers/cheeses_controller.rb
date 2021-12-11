43dab023c2f6708311ed2e969bddd3ca27e93d73
Yuyi <yuyi365@gmail.com>
2021-12-11 15:45:20 -0500

    commit tree 26767bbfa7468bcb84229801e4a629978b19342f

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,15 +1,20 @@
 class CheesesController < ApplicationController
-
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, except: %i[created_at updated_at], methods: [:summary]
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese,
+             except: %i[created_at updated_at],
+             methods: [:summary]
+    else
+      # status: :not_found will produce a 404 status code
+      render json: { error: 'Cheese not found' }, status: :not_found
+    end
   end
-
 end
